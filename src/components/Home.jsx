3800064a011a85db7d9b2ef1a068c7abd4cf7546
Abdulrahim Jamil <jameelangoabdulraheem@gmail.com>
2025-06-21 21:41:25 +0100

    build: skeleton  home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+import styles from "../styles/home.module.css";
+
+function Home(){
+    const msgs = ['one', 'two', 'three'];
+    return(
+        <div className={styles.container}>
+            <div className={styles.messages}>
+                {msgs.map((msg) => (
+                    <div className={styles.preview}>
+                        <h2>{msg}</h2>
+                        <p>12:00 PM</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,10 @@ import './index.css'
 import App from './App.jsx'
 import Welcome from './components/welcome.jsx'
 import Signup from './components/Signup.jsx'
+import Home from './components/Home.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Signup />
+    <Home />
   </StrictMode>,
 )
