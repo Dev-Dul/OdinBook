97665f5c3a8ea7b2dc56145959cebedb89260958
Abdulrahim Jamil <jameelangoabdulraheem@gmail.com>
2025-06-21 21:13:51 +0100

    build: skeleton  welcome component

---
+++ src/components/welcome.jsx
@@ -0,0 +1,25 @@
+import styles from "../styles/welcome.module.css";
+
+
+function Welcome(){
+    return (
+        <div className={styles.container}>
+            <h1>Welcome, to TreeHouse.</h1>
+            <form action="">
+                <h2>Login if you have an account</h2>
+                <div className={styles.inputBox}>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" />
+                </div>
+                <div className={styles.inputBox}>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" />
+                </div>
+                <button>Log In</button>
+                <p>Don't have an account? Sign Up</p>
+            </form>
+        </div>
+    )
+}
+
+export default Welcome;
