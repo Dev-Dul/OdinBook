0aa26ea970d0de6931f2ce6ea23df4f64bc3f205
Abdulrahim Jamil <jameelangoabdulraheem@gmail.com>
2025-06-28 17:07:54 +0100

    build: formVal and submit for login page

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -1,24 +1,62 @@
 import { useNavigate } from "react-router-dom";
 import styles from "../styles/welcome.module.css";
+import { useForm } from "react-hook-form";
+import { logIn } from "../../utils/fetch";
+import { Link } from "react-router-dom";
 
 
 function Welcome(){
     const navigate = useNavigate();
+    const {
+      register,
+      handleSubmit,
+      formState: { errors },
+    } = useForm();
+
+    async function onSubmit(formData){
+        const logInPromise = await logIn(formData.username, formData.password);
+        toast.promise(logInPromise, {
+            loading: "Just a moment...",
+            success: (response) => {
+                if(response){
+                    navigate("/home");
+                    return response.message;
+                }
+            },
+            error: (error) => {
+                return error.message;
+            }
+        })
+    }
     return (
         <div className={styles.container}>
-            <h1>Welcome, to TreeHouse.</h1>
-            <form action="">
-                <h2>Login if you have an account</h2>
+            <h1>Welcome Back to TreeHouse.</h1>
+            <form action="" onSubmit={handleSubmit(onSubmit)}>
+                <h2>Login to continue to your account.</h2>
                 <div className={styles.inputBox}>
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id="email" />
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id="username" {...register("username", {
+                        required: "Username is required",
+                        minLength: {
+                            value: 5,
+                            message: "Username must be at least 5 characters long"
+                        }
+                    })}/>
+                    {errors.username && <p>{errors.username.message}</p>}
                 </div>
                 <div className={styles.inputBox}>
                     <label htmlFor="password">Password</label>
-                    <input type="password" id="password" />
+                    <input type="password" id="password" {...register("password", {
+                        required: "Password is required",
+                        minLength: {
+                            value: 6,
+                            message: "Password must be at least 6 characters long.",
+                        }
+                    })}/>
+                    {errors.password && <p>{errors.password.message}</p>}
                 </div>
-                <button onClick={() => navigate("/signup")}>Log In</button>
-                <p>Don't have an account? Sign Up</p>
+                <button type="submit">Log In</button>
+                <p>Don't have an account? <Link to={"/signup"}>Sign Up</Link></p>
             </form>
         </div>
     )
