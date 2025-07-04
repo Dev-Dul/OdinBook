f3ad8bb8419351e8863ddad09c08817c5ee20c91
Abdulrahim Jamil <jameelangoabdulraheem@gmail.com>
2025-07-04 12:20:20 +0100

    build: image import

---
+++ utils/images.js
@@ -0,0 +1,10 @@
+import one from "../src/assets/Img/one.jpeg";
+import two from "../src/assets/Img/two.jpeg";
+import three from "../src/assets/Img/three.jpeg";
+import four from "../src/assets/Img/four.jpeg";
+import five from "../src/assets/Img/five.jpeg";
+import treehouse from "../src/assets/Img/treehouse.png";
+
+
+export const Images = [ one, two, three, four, five ];
+export default treehouse;
