bed9918c9e9c3c0e12bafd3421784f69f71b6ffc
Abdulrahim Jamil <jameelangoabdulraheem@gmail.com>
2025-07-18 11:33:25 +0100

    build: Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,18 @@
 import styles from "../styles/navbar.module.css";
-import { Home, Users, User, UserPlus} from "lucide-react";
+import { Home, Search, FeatherIcon, User, UserPlus} from "lucide-react";
 import { useNavigate, useLocation } from "react-router-dom";
 
 function Navbar({ showNav, isMobile }){
     const navigate = useNavigate();
-    const location = useLocation();
+    // const location = useLocation();
 
     return (
         <div className={`${styles.navbar} ${(!showNav && isMobile) ? styles.show  : ''}`}>
-            <Home className={styles.icon} style={{ '--i': '0'}} onClick={() => navigate('/home')}/>
-            <Users className={styles.icon} style={{ '--i': '1'}} onClick={() => navigate('/nests')}/>
-            <UserPlus className={styles.icon} style={{ '--i': '2'}} onClick={() => navigate('/friends')}/>
-            <User className={styles.icon} style={{ '--i': '3'}} onClick={() => navigate('/profile')}/>
+            <Home className={styles.icon} style={{ '--i': '1'}} onClick={() => navigate('/home')}/>
+            <Search className={styles.icon} style={{ '--i': '2'}} onClick={() => navigate('/nests')}/>
+            <FeatherIcon className={styles.icon} style={{ '--i': '3'}} onClick={() => navigate('/nests')}/>
+            <UserPlus className={styles.icon} style={{ '--i': '4'}} onClick={() => navigate('/friends')}/>
+            <User className={styles.icon} style={{ '--i': '5'}} onClick={() => navigate('/profile')}/>
         </div>
     )
 }
