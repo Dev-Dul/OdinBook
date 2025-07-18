6f75d114689bb7b7267c082ddc34dc70a5e115cc
Abdulrahim Jamil <jameelangoabdulraheem@gmail.com>
2025-07-18 11:29:55 +0100

    build: Sidebar Component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import styles from "../styles/sidebar.module.css";
-import { Home, Users, User, UserPlus, Menu, ArrowLeft } from "lucide-react";
+import { Home, Search, Plus, FeatherIcon, User, UserPlus, Menu, ArrowLeft } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 
 function Sidebar(){
-    const [collapse, setCollapse] = useState(false);
-    const navigate = useNavigate();
+    const [collapse, setCollapse] = useState(true);
+    // const navigate = useNavigate();
     const items = [
         { icon: <Home />, label: "Home", path: '/home' },
-        { icon: <Users />, label: "Nests", path: '/nests' },
+        { icon: <Search />, label: "Search", path: '/nests' },
+        { icon: <FeatherIcon />, label: "Post", path: '/nests' },
         { icon: <UserPlus />, label: "Friends", path: '/friends' },
         { icon: <User />, label: "Profile", path: '/profile' },
     ]
@@ -19,17 +20,26 @@ function Sidebar(){
 
     return (
       <div className={`${styles.sidebar} ${collapse ? styles.collapsed : ''}`}>
-        <div className={`${styles.icon} ${styles.one}`} onClick={handleCollapse}>
+        <div className="top">
+          {collapse ? <h2>O</h2> : <h2>OdinBook</h2> }
+        </div>
+        <div className="middle">
+          {items.map((item, index) => (
+            <div className={styles.icon} key={index} 
+              onClick={() => navigate(item.path)}
+              style={{ borderBottom: location.pathname === item.path ? "2px solid" : '' }}>
+              {item.icon}
+              {!collapse && item.label}
+            </div>
+        ))}
+        </div>
+        <div className="bottom">
+          <div className={`${styles.icon} ${styles.one}`} onClick={handleCollapse}>
           {!collapse ? <ArrowLeft />
            : <Menu />
           }
         </div>
-        {items.map((item, index) => (
-          <div className={styles.icon} key={index} onClick={() => navigate(item.path)}>
-            {item.icon}
-            {!collapse && item.label}
-          </div>
-        ))}
+        </div>
       </div>
     );
 }
